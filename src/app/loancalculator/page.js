ef0e72ffaedc07f0df96777851c7770ce8510356
globussolution <aminhocain@gmail.com>
2025-01-26 07:55:41 +0500

    resolving deployment errors

--- src/app/loancalculator/page.js
+++ src/app/loancalculator/page.js
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 import LoanCalculator from "@/components/LoanCalculator";
 
 export default function Loancalculator() {
   return (
     <div className="mx-auto p-20 h-">
+      <Header />
       <LoanCalculator />
     </div>
   );
